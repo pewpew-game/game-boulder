36893b8911730f7ed8cebfb3249efad5a8a8fc2c
Radomir Dopieralski <openstack@sheep.art.pl>
2019-07-07 10:30:16 +0200

    Add gems and starting position

--- boulder.py
+++ boulder.py
@@ -3,19 +3,25 @@ import pew
 
 pew.init()
 screen = pew.Pix.from_iter((
-    (1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 1, 1, 4, 1, 1, 1, 1),
     (1, 2, 2, 2, 3, 0, 2, 1),
-    (1, 1, 3, 1, 2, 2, 2, 1),
+    (1, 1, 3, 1, 2, 5, 2, 1),
     (1, 2, 2, 1, 2, 1, 1, 1),
     (1, 2, 0, 3, 2, 3, 2, 1),
-    (1, 2, 0, 1, 2, 2, 2, 1),
+    (1, 2, 0, 5, 2, 2, 2, 1),
     (1, 2, 2, 2, 2, 2, 2, 1),
     (1, 1, 1, 1, 1, 1, 1, 1),
 ))
-x = 1
-y = 1
+
 blink = 1
 dead = False
+for y in range(8):
+    for x in range(8):
+        if screen.pixel(x, y) == 4:
+            break;
+    else:
+        continue
+    break
 
 while not dead:
     screen.pixel(x, y, 0)
@@ -30,7 +36,7 @@ while not dead:
         dx = -1
     elif pressed & pew.K_RIGHT:
         dx = 1
-    if screen.pixel(x + dx, y + dy) in {0, 2}:
+    if screen.pixel(x + dx, y + dy) in {0, 2, 5, 6}:
         x += dx
         y += dy
     elif screen.pixel(x + dx, y + dy) == 3 and dy == 0:
@@ -49,11 +55,12 @@ while not dead:
                 screen.pixel(a, b + 1, 3)
                 if (a, b + 2) == (x, y):
                     dead = True
-            if screen.pixel(a, b) == 2:
+            if screen.pixel(a, b) in {5, 6}:
+                screen.pixel(a, b, 5 if blink else 6)
                 count += 1
     if count == 0:
         break
-    screen.pixel(x, y, 2 + blink)
+    screen.pixel(x, y, 3 if blink else 0)
     blink = 0 if blink else 1
     pew.show(screen)
     pew.tick(1 / 6)

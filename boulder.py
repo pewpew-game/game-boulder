4aab115e99f727820eb1302e5c48cd1f9a9d6306
Radomir Dopieralski <openstack@sheep.art.pl>
2019-07-07 10:35:41 +0200

    Use names for map features

--- boulder.py
+++ boulder.py
@@ -1,6 +1,15 @@
 import pew
 
 
+VOID = 0
+WALL = 1
+SOIL = 2
+ROCK = 3
+HERO = 4
+GEM1 = 5
+GEM2 = 6
+
+
 pew.init()
 screen = pew.Pix.from_iter((
     (1, 1, 1, 4, 1, 1, 1, 1),
@@ -24,7 +33,7 @@ for y in range(8):
     break
 
 while not dead:
-    screen.pixel(x, y, 0)
+    screen.pixel(x, y, VOID)
     pressed = pew.keys()
     dx = 0
     dy = 0
@@ -36,31 +45,31 @@ while not dead:
         dx = -1
     elif pressed & pew.K_RIGHT:
         dx = 1
-    if screen.pixel(x + dx, y + dy) in {0, 2, 5, 6}:
+    if screen.pixel(x + dx, y + dy) not in {WALL, ROCK}:
         x += dx
         y += dy
-    elif screen.pixel(x + dx, y + dy) == 3 and dy == 0:
-        if screen.pixel(x + dx + dx, y + dy + dy) == 0:
-            screen.pixel(x + dx + dx, y + dy + dy, 3)
-            screen.pixel(x + dx, y + dy, 0)
+    elif screen.pixel(x + dx, y + dy) == ROCK and dy == 0:
+        if screen.pixel(x + dx + dx, y + dy + dy) == VOID:
+            screen.pixel(x + dx + dx, y + dy + dy, ROCK)
+            screen.pixel(x + dx, y + dy, VOID)
             x += dx
             y += dy
     count = 0
     for a in range(8):
         for b in range(7, -1, -1):
-            if (screen.pixel(a, b) == 3 and
-                    screen.pixel(a, b + 1) == 0 and
+            if (screen.pixel(a, b) == ROCK and
+                    screen.pixel(a, b + 1) == VOID and
                     (a, b + 1) != (x, y)):
-                screen.pixel(a, b, 0)
-                screen.pixel(a, b + 1, 3)
+                screen.pixel(a, b, VOID)
+                screen.pixel(a, b + 1, ROCK)
                 if (a, b + 2) == (x, y):
                     dead = True
-            if screen.pixel(a, b) in {5, 6}:
-                screen.pixel(a, b, 5 if blink else 6)
+            if screen.pixel(a, b) in {GEM1, GEM2}:
+                screen.pixel(a, b, GEM1 if blink else GEM2)
                 count += 1
     if count == 0:
         break
-    screen.pixel(x, y, 3 if blink else 0)
-    blink = 0 if blink else 1
+    screen.pixel(x, y, 3 if blink else HERO)
+    blink = not blink
     pew.show(screen)
     pew.tick(1 / 6)
